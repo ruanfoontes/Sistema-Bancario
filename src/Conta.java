2d5f8a0083af5ecf4b4a70b78ec339d443c6028e
Ruan fontes <ruanfontes309@gmail.com>
2026-03-14 11:50:29 -0300

    Criado metodo para ver extrato

--- src/Conta.java
+++ src/Conta.java
@@ -1,30 +1,42 @@
+import java.util.ArrayList;
+
 public class Conta {
 
-   private String titular;
+    private String titular;
    private double saldo;
-   private int extrato;
+   private ArrayList<String> extrato;
 
-     //Método Construtor
+       //Método Construtor
     public Conta(String titular) {
         this.titular = titular;
         this.saldo = 0;
-        this.extrato = 0;
+        this.extrato = new ArrayList<>();
     }
           //Método de depósito
     public void depositar(double valor) {
         saldo += valor;
+        extrato.add("Depósito: +" + valor);
     }
-    // Método saque
+        // Método saque
     public void sacar(double valor){
         if (saldo >= valor) {
             saldo -= valor;
+            extrato.add("Saque: -" + valor);
         } else {
             System.out.println("Saldo Insuficiente");
         }
     }
-    //método ver saldo
+        //método ver saldo
     public double getsaldo() {
         return saldo;
+    }
+    //Método Mostrar Extrato
+    public void mostrarExtrato() {
+        System.out.println("*** Extrato ***");
+
+        for (String operacao : extrato) {
+            System.out.println(operacao);
+        }
     }
 
 }
